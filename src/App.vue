85b98f0ca1208d4420497d84fb47196b80a5cf7f
llzgdc <2807349203@qq.com>
2025-04-04 20:44:14 +0800

    V3.0.0(Developing)

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <ArchivesList />
+  </div>
+</template>
+
+<script>
+import ArchivesList from './components/ArchivesList.vue';
+
+export default {
+  components: {
+    ArchivesList
+  }
+}
+</script>
+
+<style>
+@font-face {
+  font-family: 'NotoSansSC';
+  src: url('./assets/fonts/NotoSansSC-VariableFont_wght.ttf') format('truetype-variations');
+  font-weight: 100 900;
+  font-style: normal;
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
+
+body {
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  font-family: 'NotoSansSC', sans-serif;
+}
+
+* {
+  margin: 0px;
+  padding: 0px
+}
+</style>

45aa80e0ded711f656d11bfd93ea332f131ec57b
llzgdc <2807349203@qq.com>
2025-07-04 13:25:28 +0800

    feat(sidebar): add sidebar component for better navigation 🧭

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup>
 import { ref } from "vue";
 import Home from "./views/Home.vue";
+import LivingGlassSidebar from "./components/LG_SideBar.vue"; // 修改为正确变量名
+
+// 组件内方法可以直接定义为函数
+const handleItemSelected = (item) => {
+  console.log('选中的菜单项:', item);
+  // 在这里处理页面跳转或内容更新
+};
 </script>
 
 <template>
   <main class="container">
+    <LivingGlassSidebar @item-selected="handleItemSelected" />
     <Home></Home>
   </main>
 </template>
